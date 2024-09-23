9cfc5cdd842eec4d1fb40fd50779db05212a24de
taqu <taqu2920.dev@gmail.com>
2024-09-23 22:04:44 +0900

    add external
    remove oneDNN settings

--- include/cppgpt.h
+++ include/cppgpt.h
@@ -1,9 +1,8 @@
 #ifndef INC_CPPGPT_H_
 #define INC_CPPGPT_H_
-#include <cstdint>
+#include "gguf.h"
 #include <cassert>
-#include <oneapi/dnnl/dnnl.hpp>
-#include <vector>
+#include <cstdint>
 
 namespace cppgpt
 {
@@ -19,18 +18,222 @@ using u64 = uint64_t;
 using f32 = float;
 using f64 = double;
 
+using ggml_type = gguf::ggml_type;
+
+static constexpr u32 GGML_MAX_DIMS = 4;
+static constexpr u32 GGML_MAX_PARAMS = 2048;
+static constexpr u32 GGML_MAX_NAME = 64;
+static constexpr u32 GGML_MAX_SRC = 16;
+static constexpr u32 GGML_MAX_OP_PARAMS = 64;
+
+void* allocate(size_t size, size_t align=16);
+void deallocate(void* ptr, size_t align=16);
+
+// available tensor operations
+enum class ggml_op
+{
+    GGML_OP_NONE = 0,
+
+    GGML_OP_DUP,
+    GGML_OP_ADD,
+    GGML_OP_ADD1,
+    GGML_OP_ACC,
+    GGML_OP_SUB,
+    GGML_OP_MUL,
+    GGML_OP_DIV,
+    GGML_OP_SQR,
+    GGML_OP_SQRT,
+    GGML_OP_LOG,
+    GGML_OP_SIN,
+    GGML_OP_COS,
+    GGML_OP_SUM,
+    GGML_OP_SUM_ROWS,
+    GGML_OP_MEAN,
+    GGML_OP_ARGMAX,
+    GGML_OP_REPEAT,
+    GGML_OP_REPEAT_BACK,
+    GGML_OP_CONCAT,
+    GGML_OP_SILU_BACK,
+    GGML_OP_NORM, // normalize
+    GGML_OP_RMS_NORM,
+    GGML_OP_RMS_NORM_BACK,
+    GGML_OP_GROUP_NORM,
+
+    GGML_OP_MUL_MAT,
+    GGML_OP_MUL_MAT_ID,
+    GGML_OP_OUT_PROD,
+
+    GGML_OP_SCALE,
+    GGML_OP_SET,
+    GGML_OP_CPY,
+    GGML_OP_CONT,
+    GGML_OP_RESHAPE,
+    GGML_OP_VIEW,
+    GGML_OP_PERMUTE,
+    GGML_OP_TRANSPOSE,
+    GGML_OP_GET_ROWS,
+    GGML_OP_GET_ROWS_BACK,
+    GGML_OP_DIAG,
+    GGML_OP_DIAG_MASK_INF,
+    GGML_OP_DIAG_MASK_ZERO,
+    GGML_OP_SOFT_MAX,
+    GGML_OP_SOFT_MAX_BACK,
+    GGML_OP_ROPE,
+    GGML_OP_ROPE_BACK,
+    GGML_OP_CLAMP,
+    GGML_OP_CONV_TRANSPOSE_1D,
+    GGML_OP_IM2COL,
+    GGML_OP_IM2COL_BACK,
+    GGML_OP_CONV_TRANSPOSE_2D,
+    GGML_OP_POOL_1D,
+    GGML_OP_POOL_2D,
+    GGML_OP_POOL_2D_BACK,
+    GGML_OP_UPSCALE, // nearest interpolate
+    GGML_OP_PAD,
+    GGML_OP_ARANGE,
+    GGML_OP_TIMESTEP_EMBEDDING,
+    GGML_OP_ARGSORT,
+    GGML_OP_LEAKY_RELU,
+
+    GGML_OP_FLASH_ATTN_EXT,
+    GGML_OP_FLASH_ATTN_BACK,
+    GGML_OP_SSM_CONV,
+    GGML_OP_SSM_SCAN,
+    GGML_OP_WIN_PART,
+    GGML_OP_WIN_UNPART,
+    GGML_OP_GET_REL_POS,
+    GGML_OP_ADD_REL_POS,
+    GGML_OP_RWKV_WKV,
+
+    GGML_OP_UNARY,
+
+    GGML_OP_MAP_UNARY,
+    GGML_OP_MAP_BINARY,
+
+    GGML_OP_MAP_CUSTOM1_F32,
+    GGML_OP_MAP_CUSTOM2_F32,
+    GGML_OP_MAP_CUSTOM3_F32,
+
+    GGML_OP_MAP_CUSTOM1,
+    GGML_OP_MAP_CUSTOM2,
+    GGML_OP_MAP_CUSTOM3,
+
+    GGML_OP_CROSS_ENTROPY_LOSS,
+    GGML_OP_CROSS_ENTROPY_LOSS_BACK,
+
+    GGML_OP_COUNT,
+};
+
+//--- Memory
+//-----------------------------------------------------------
+class Memory
+{
+public:
+    Memory();
+    explicit Memory(u64 size);
+    Memory(Memory&& other);
+    ~Memory();
+    Memory& operator=(Memory&& other);
+
+    u64 size() const
+    {
+        return size_;
+    }
+
+    template<class T>
+    operator const T*() const
+    {
+        return reinterpret_cast<const T*>(ptr_);
+    }
+
+    template<class T>
+    operator T*()
+    {
+        return reinterpret_cast<T*>(ptr_);
+    }
+
+private:
+    Memory(const Memory&) = delete;
+    Memory& operator=(const Memory&) = delete;
+    u64 size_;
+    void* ptr_;
+};
+
+//--- MemoryView
+//-----------------------------------------------------------
+class MemoryView
+{
+public:
+    MemoryView();
+    explicit MemoryView(ggml_type type, u64 size, const void* ptr);
+    MemoryView(MemoryView&& other);
+    ~MemoryView();
+    MemoryView& operator=(MemoryView&& other);
+
+    u64 size() const;
+
+    u32 operator[](u64 i) const;
+
+private:
+    MemoryView(const MemoryView&) = delete;
+    MemoryView& operator=(const MemoryView&) = delete;
+    ggml_type type_;
+    u32 bits_;
+    u64 size_;
+    const void* ptr_;
+};
+
+//--- Tensor
+//-----------------------------------------------------------
+class Tensor
+{
+public:
+    Tensor();
+    Tensor(ggml_type type, u64 dimensions);
+    ~Tensor();
+    void zeros();
+    void ones();
+
+private:
+    Tensor(const Tensor&) = delete;
+    Tensor& operator=(const Tensor&) = delete;
+    ggml_type type_;
+    Memory data_;
+};
+
 //--- Module
 //-----------------------------------------------------------
 class Module
 {
 public:
+    virtual ~Module()
+    {
+    }
+    virtual dnnl::memory forward(dnnl::memory tensor) = 0;
+
 protected:
-	Module();
-	virtual ~Module();
-	virtual dnnl::memory forward(dnnl::memory tensor, uint32_t start_pos, dnnl::memory freqs_cis, dnnl::memory mask);
+    Module()
+    {
+    }
+
 private:
-	Module(const Module&) = delete;
-	Module& operator=(const Module&) = delete;
+    Module(const Module&) = delete;
+    Module& operator=(const Module&) = delete;
+};
+
+//--- RMSNorm
+//-----------------------------------------------------------
+class RMSNorm: public Module
+{
+public:
+    RMSNorm(ggml_type type, u32 dimensions, f32 epsilon = 1.0e-6);
+    virtual ~RMSNorm();
+    virtual dnnl::memory forward(dnnl::memory tensor) override;
+
+private:
+    dnnl::memory norm(dnnl::memory x);
+    f32 epsilon_;
+    Tensor weight_;
 };
 
 //--- Model
@@ -39,47 +242,50 @@ class Model
 {
 public:
 protected:
-	Model();
-	virtual ~Model();
+    Model();
+    virtual ~Model();
+
 private:
-	Model(const Model&) = delete;
-	Model& operator=(const Model&) = delete;
+    Model(const Model&) = delete;
+    Model& operator=(const Model&) = delete;
 };
 
 //--- Llama2
 //-----------------------------------------------------------
-class Llama2 : public virtual Model
+class Llama2: public virtual Model
 {
 public:
 protected:
-	/*
-	llama_configs = {
+    /*
+    llama_configs = {
     "7B": dict(n_layer=32, n_head=32, n_embd=4096),
     "13B": dict(n_layer=40, n_head=40, n_embd=5120),
     "30B": dict(n_layer=60, n_head=52, n_embd=6656),
     "65B": dict(n_layer=80, n_head=64, n_embd=8192),
 }
-	*/
-	struct Config
+    */
+    struct Config
     {
-		uint32_t block_size_ = 2048;
-		uint32_t vocab_size_ = 32000;
-		uint32_t padding_vocab_size_ = 0;
-		uint32_t n_layer_ = 32;
-		uint32_t n_head_ = 32;
-		uint32_t n_embd_ = 4096;
+        uint32_t block_size_ = 2048;
+        uint32_t vocab_size_ = 32000;
+        uint32_t padding_vocab_size_ = 0;
+        uint32_t n_layer_ = 32;
+        uint32_t n_head_ = 32;
+        uint32_t n_embd_ = 4096;
     };
 
-	Llama2();
-	explicit Llama2(const Config& config);
-	Llama2(Llama2&& other);
-	virtual ~Llama2();
-	Llama2& operator=(Llama2&& other);
+    Llama2();
+    explicit Llama2(const Config& config);
+    Llama2(Llama2&& other);
+    virtual ~Llama2();
+    Llama2& operator=(Llama2&& other);
+
 private:
-	Llama2(const Llama2&) = delete;
-	Llama2& operator=(const Llama2&) = delete;
-	Config config_;
-	std::vector<dnnl::primitive> network_;
+    Llama2(const Llama2&) = delete;
+    Llama2& operator=(const Llama2&) = delete;
+    Config config_;
+    std::vector<dnnl::primitive> network_;
+    std::vector<std::unordered_map<int32_t, dnnl::memory>> args_;
 };
 } // namespace cppgpt
 #endif // INC_CPPGPT_H_
--- src/cppgpt.cpp
+++ src/cppgpt.cpp
@@ -1,7 +1,536 @@
 #include "cppgpt.h"
+#include <mimalloc-2.1/mimalloc.h>
 
 namespace cppgpt
 {
+namespace
+{
+    u32 get_bit_size(ggml_type type)
+    {
+        switch(type) {
+        case ggml_type::GGML_TYPE_F32:
+            return 32;
+        case ggml_type::GGML_TYPE_F16:
+            return 16;
+        case ggml_type::GGML_TYPE_Q4_0:
+            return 4;
+        case ggml_type::GGML_TYPE_Q4_1:
+            return 4;
+        case ggml_type::GGML_TYPE_Q5_0:
+            return 5;
+        case ggml_type::GGML_TYPE_Q5_1:
+            return 5;
+        case ggml_type::GGML_TYPE_Q8_0:
+            return 8;
+        case ggml_type::GGML_TYPE_Q8_1:
+            return 8;
+        case ggml_type::GGML_TYPE_Q2_K:
+            return 2;
+        case ggml_type::GGML_TYPE_Q3_K:
+            return 3;
+        case ggml_type::GGML_TYPE_Q4_K:
+            return 4;
+        case ggml_type::GGML_TYPE_Q5_K:
+            return 5;
+        case ggml_type::GGML_TYPE_Q6_K:
+            return 6;
+        case ggml_type::GGML_TYPE_Q8_K:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ2_XXS:
+            return 2;
+        case ggml_type::GGML_TYPE_IQ2_XS:
+            return 2;
+        case ggml_type::GGML_TYPE_IQ3_XXS:
+            return 3;
+        case ggml_type::GGML_TYPE_IQ1_S:
+            return 1;
+        case ggml_type::GGML_TYPE_IQ4_NL:
+            return 4;
+        case ggml_type::GGML_TYPE_IQ3_S:
+            return 3;
+        case ggml_type::GGML_TYPE_IQ2_S:
+            return 2;
+        case ggml_type::GGML_TYPE_IQ4_XS:
+            return 4;
+        case ggml_type::GGML_TYPE_I8:
+            return 8;
+        case ggml_type::GGML_TYPE_I16:
+            return 16;
+        case ggml_type::GGML_TYPE_I32:
+            return 32;
+        case ggml_type::GGML_TYPE_I64:
+            return 64;
+        case ggml_type::GGML_TYPE_F64:
+            return 64;
+        case ggml_type::GGML_TYPE_IQ1_M:
+            return 1;
+        default:
+            assert(false);
+            return 0;
+        }
+    }
+
+    u32 get_bit_size_aligned(ggml_type type)
+    {
+        switch(type) {
+        case ggml_type::GGML_TYPE_F32:
+            return 32;
+        case ggml_type::GGML_TYPE_F16:
+            return 16;
+        case ggml_type::GGML_TYPE_Q4_0:
+            return 8;
+        case ggml_type::GGML_TYPE_Q4_1:
+            return 8;
+        case ggml_type::GGML_TYPE_Q5_0:
+            return 8;
+        case ggml_type::GGML_TYPE_Q5_1:
+            return 8;
+        case ggml_type::GGML_TYPE_Q8_0:
+            return 8;
+        case ggml_type::GGML_TYPE_Q8_1:
+            return 8;
+        case ggml_type::GGML_TYPE_Q2_K:
+            return 8;
+        case ggml_type::GGML_TYPE_Q3_K:
+            return 8;
+        case ggml_type::GGML_TYPE_Q4_K:
+            return 8;
+        case ggml_type::GGML_TYPE_Q5_K:
+            return 8;
+        case ggml_type::GGML_TYPE_Q6_K:
+            return 8;
+        case ggml_type::GGML_TYPE_Q8_K:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ2_XXS:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ2_XS:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ3_XXS:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ1_S:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ4_NL:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ3_S:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ2_S:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ4_XS:
+            return 8;
+        case ggml_type::GGML_TYPE_I8:
+            return 8;
+        case ggml_type::GGML_TYPE_I16:
+            return 16;
+        case ggml_type::GGML_TYPE_I32:
+            return 32;
+        case ggml_type::GGML_TYPE_I64:
+            return 64;
+        case ggml_type::GGML_TYPE_F64:
+            return 64;
+        case ggml_type::GGML_TYPE_IQ1_M:
+            return 8;
+        default:
+            assert(false);
+            return 0;
+        }
+    }
+
+    u32 get_byte_size_aligned(ggml_type type)
+    {
+        switch(type) {
+        case ggml_type::GGML_TYPE_F32:
+            return 4;
+        case ggml_type::GGML_TYPE_F16:
+            return 2;
+        case ggml_type::GGML_TYPE_Q4_0:
+            return 1;
+        case ggml_type::GGML_TYPE_Q4_1:
+            return 1;
+        case ggml_type::GGML_TYPE_Q5_0:
+            return 1;
+        case ggml_type::GGML_TYPE_Q5_1:
+            return 1;
+        case ggml_type::GGML_TYPE_Q8_0:
+            return 1;
+        case ggml_type::GGML_TYPE_Q8_1:
+            return 1;
+        case ggml_type::GGML_TYPE_Q2_K:
+            return 1;
+        case ggml_type::GGML_TYPE_Q3_K:
+            return 1;
+        case ggml_type::GGML_TYPE_Q4_K:
+            return 1;
+        case ggml_type::GGML_TYPE_Q5_K:
+            return 1;
+        case ggml_type::GGML_TYPE_Q6_K:
+            return 1;
+        case ggml_type::GGML_TYPE_Q8_K:
+            return 1;
+        case ggml_type::GGML_TYPE_IQ2_XXS:
+            return 1;
+        case ggml_type::GGML_TYPE_IQ2_XS:
+            return 1;
+        case ggml_type::GGML_TYPE_IQ3_XXS:
+            return 1;
+        case ggml_type::GGML_TYPE_IQ1_S:
+            return 1;
+        case ggml_type::GGML_TYPE_IQ4_NL:
+            return 1;
+        case ggml_type::GGML_TYPE_IQ3_S:
+            return 1;
+        case ggml_type::GGML_TYPE_IQ2_S:
+            return 1;
+        case ggml_type::GGML_TYPE_IQ4_XS:
+            return 1;
+        case ggml_type::GGML_TYPE_I8:
+            return 1;
+        case ggml_type::GGML_TYPE_I16:
+            return 2;
+        case ggml_type::GGML_TYPE_I32:
+            return 4;
+        case ggml_type::GGML_TYPE_I64:
+            return 8;
+        case ggml_type::GGML_TYPE_F64:
+            return 8;
+        case ggml_type::GGML_TYPE_IQ1_M:
+            return 1;
+        default:
+            assert(false);
+            return 0;
+        }
+    }
+
+    u32 get(u32 bits, void* data)
+    {
+    }
+} // namespace
+
+void* allocate(size_t size, size_t align)
+{
+    return mi_malloc_aligned(size, align);
+}
+
+void deallocate(void* ptr, size_t align)
+{
+    mi_free_aligned(ptr, align);
+}
+
+//--- Memory
+//-----------------------------------------------------------
+
+Memory::Memory()
+    :size_(0)
+    ,ptr_(nullptr)
+{
+}
+
+Memory::Memory(Memory&& other)
+    :size_(other.size_)
+    ,ptr_(other.ptr_)
+{
+    other.size_ = 0;
+    other.ptr_ = nullptr;
+}
+
+Memory::Memory(u64 size)
+    :size_(size)
+    ,ptr_(nullptr)
+{
+}
+
+Memory::~Memory()
+{
+    deallocate(ptr_);
+    ptr_ = nullptr;
+}
+
+Memory& Memory::operator=(Memory&& other)
+{
+    if(this != &other){
+        deallocate(ptr_);
+        size_ = other.size_;
+        ptr_ = other.ptr_;
+        other.size_ = 0;
+        other.ptr_ = nullptr;
+    }
+    return *this;
+}
+
+//--- MemoryView
+//-----------------------------------------------------------
+MemoryView::MemoryView()
+    :type_(ggml_type::GGML_TYPE_F32)
+    ,bits_(0)
+    ,size_(0)
+    ,ptr_(nullptr)
+{
+}
+
+    MemoryView::MemoryView(ggml_type type, u64 size, const void* ptr)
+    :type_(type)
+        ,bits_(get_bit_size(type))
+        ,size_(size)
+        ,ptr_(ptr)
+{
+        assert(nullptr != ptr);
+}
+
+    MemoryView::MemoryView(MemoryView&& other)
+{
+}
+
+    MemoryView::~MemoryView()
+{
+}
+
+    MemoryView& MemoryView::operator=(MemoryView&& other)
+{
+        if(this != &other){
+            type_ = other.type_;
+            bits_ = other.bits_;
+            size_ = other.size_;
+            ptr_ = other.ptr_;
+        }
+        return *this;
+}
+
+    u64 MemoryView::size() const
+{
+        return size_;
+}
+
+u32 MemoryView::operator[](u64 i) const
+{
+    assert(i < size_);
+    u64 total_bits = bits_ * size_;
+    switch(type_) {
+    case ggml_type::GGML_TYPE_F32:
+        return 4;
+    case ggml_type::GGML_TYPE_F16:
+        return 2;
+    case ggml_type::GGML_TYPE_Q4_0:
+        return 1;
+    case ggml_type::GGML_TYPE_Q4_1:
+        return 1;
+    case ggml_type::GGML_TYPE_Q5_0:
+        return 1;
+    case ggml_type::GGML_TYPE_Q5_1:
+        return 1;
+    case ggml_type::GGML_TYPE_Q8_0:
+        return 1;
+    case ggml_type::GGML_TYPE_Q8_1:
+        return 1;
+    case ggml_type::GGML_TYPE_Q2_K:
+        return 1;
+    case ggml_type::GGML_TYPE_Q3_K:
+        return 1;
+    case ggml_type::GGML_TYPE_Q4_K:
+        return 1;
+    case ggml_type::GGML_TYPE_Q5_K:
+        return 1;
+    case ggml_type::GGML_TYPE_Q6_K:
+        return 1;
+    case ggml_type::GGML_TYPE_Q8_K:
+        return 1;
+    case ggml_type::GGML_TYPE_IQ2_XXS:
+        return 1;
+    case ggml_type::GGML_TYPE_IQ2_XS:
+        return 1;
+    case ggml_type::GGML_TYPE_IQ3_XXS:
+        return 1;
+    case ggml_type::GGML_TYPE_IQ1_S:
+        return 1;
+    case ggml_type::GGML_TYPE_IQ4_NL:
+        return 1;
+    case ggml_type::GGML_TYPE_IQ3_S:
+        return 1;
+    case ggml_type::GGML_TYPE_IQ2_S:
+        return 1;
+    case ggml_type::GGML_TYPE_IQ4_XS:
+        return 1;
+    case ggml_type::GGML_TYPE_I8:
+        return 1;
+    case ggml_type::GGML_TYPE_I16:
+        return 2;
+    case ggml_type::GGML_TYPE_I32:
+        return 4;
+    case ggml_type::GGML_TYPE_I64:
+        return 8;
+    case ggml_type::GGML_TYPE_F64:
+        return 8;
+    case ggml_type::GGML_TYPE_IQ1_M:
+        return 1;
+    default:
+        assert(false);
+        return 0;
+    }
+}
+
+//--- Tensor
+//-----------------------------------------------------------
+namespace
+{
+#if 0
+    void write_to_dnnl_memory(dnnl::memory& mem, void* src)
+    {
+        assert(mem);
+        assert(nullptr != src);
+        dnnl::engine eng = mem.get_engine();
+        size_t size = mem.get_desc().get_size();
+#ifdef DNNL_WITH_SYCL
+        bool is_cpu_sycl = (DNNL_CPU_RUNTIME == DNNL_RUNTIME_SYCL
+                            && eng.get_kind() == dnnl::engine::kind::cpu);
+        bool is_gpu_sycl = (DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
+                            && eng.get_kind() == dnnl::engine::kind::gpu);
+        if(is_cpu_sycl || is_gpu_sycl) {
+            auto mkind = dnnl::sycl_interop::get_memory_kind(mem);
+            if(mkind == dnnl::sycl_interop::memory_kind::buffer) {
+                auto buffer = dnnl::sycl_interop::get_buffer<uint8_t>(mem);
+                auto dst = buffer.get_host_access();
+                uint8_t* dst_ptr = dst.get_pointer();
+                if(!dst_ptr)
+                    throw std::runtime_error("get_pointer returned nullptr.");
+                for(size_t i = 0; i < size; ++i)
+                    dst_ptr[i] = ((uint8_t*)handle)[i];
+            } else {
+                assert(mkind == dnnl::sycl_interop::memory_kind::usm);
+                uint8_t* dst_ptr = (uint8_t*)mem.get_data_handle();
+                if(!dst_ptr)
+                    throw std::runtime_error("get_data_handle returned nullptr.");
+                if(is_cpu_sycl) {
+                    for(size_t i = 0; i < size; ++i)
+                        dst_ptr[i] = ((uint8_t*)handle)[i];
+                } else {
+                    auto sycl_queue = dnnl::sycl_interop::get_queue(dnnl::stream(eng));
+                    sycl_queue.memcpy(dst_ptr, handle, size).wait();
+                }
+            }
+            return;
+        }
+#endif
+#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+        if(eng.get_kind() == dnnl::engine::kind::gpu) {
+            void* mapped_ptr = mem.map_data();
+            if(nullptr != mapped_ptr) {
+                ::memcpy(mapped_ptr, src, size);
+            }
+            mem.unmap_data(mapped_ptr);
+            return;
+        }
+#endif
+
+        if(eng.get_kind() == dnnl::engine::kind::cpu) {
+            uint8_t* dst = static_cast<uint8_t*>(mem.get_data_handle());
+            if(nullptr != dst) {
+                ::memcpy(dst, src, size);
+            }
+            return;
+        }
+    }
+
+    void memset_dnnl_memory(dnnl::memory& mem, int32_t x)
+    {
+        assert(mem);
+        dnnl::engine eng = mem.get_engine();
+        size_t size = mem.get_desc().get_size();
+#ifdef DNNL_WITH_SYCL
+        bool is_cpu_sycl = (DNNL_CPU_RUNTIME == DNNL_RUNTIME_SYCL
+                            && eng.get_kind() == dnnl::engine::kind::cpu);
+        bool is_gpu_sycl = (DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
+                            && eng.get_kind() == dnnl::engine::kind::gpu);
+        if(is_cpu_sycl || is_gpu_sycl) {
+            auto mkind = dnnl::sycl_interop::get_memory_kind(mem);
+            if(mkind == dnnl::sycl_interop::memory_kind::buffer) {
+                auto buffer = dnnl::sycl_interop::get_buffer<uint8_t>(mem);
+                auto dst = buffer.get_host_access();
+                uint8_t* dst_ptr = dst.get_pointer();
+                if(!dst_ptr)
+                    throw std::runtime_error("get_pointer returned nullptr.");
+                for(size_t i = 0; i < size; ++i)
+                    dst_ptr[i] = ((uint8_t*)handle)[i];
+            } else {
+                assert(mkind == dnnl::sycl_interop::memory_kind::usm);
+                uint8_t* dst_ptr = (uint8_t*)mem.get_data_handle();
+                if(!dst_ptr)
+                    throw std::runtime_error("get_data_handle returned nullptr.");
+                if(is_cpu_sycl) {
+                    for(size_t i = 0; i < size; ++i)
+                        dst_ptr[i] = ((uint8_t*)handle)[i];
+                } else {
+                    auto sycl_queue = dnnl::sycl_interop::get_queue(dnnl::stream(eng));
+                    sycl_queue.memcpy(dst_ptr, handle, size).wait();
+                }
+            }
+            return;
+        }
+#endif
+#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+        if(eng.get_kind() == dnnl::engine::kind::gpu) {
+            void* mapped_ptr = mem.map_data();
+            if(nullptr != mapped_ptr) {
+                ::memset(mapped_ptr, x, size);
+            }
+            mem.unmap_data(mapped_ptr);
+            return;
+        }
+#endif
+
+        if(eng.get_kind() == dnnl::engine::kind::cpu) {
+            uint8_t* dst = static_cast<uint8_t*>(mem.get_data_handle());
+            if(nullptr != dst) {
+                ::memset(dst, 0, size);
+            }
+            return;
+        }
+    }
+#endif
+} // namespace
+
+Tensor::Tensor()
+    : type_(ggml_type::GGML_TYPE_F32)
+{
+}
+
+Tensor::Tensor(ggml_type type, u64 dimensions)
+    : type_(type)
+    , data_(get_byte_size_aligned(type)*dimensions)
+{
+}
+
+Tensor::~Tensor()
+{
+}
+
+void Tensor::zeros()
+{
+    memset_dnnl_memory(data_, 0);
+}
+
+void Tensor::ones()
+{
+    memset_dnnl_memory(data_, 1);
+}
+
+//--- RMSNorm
+//-----------------------------------------------------------
+RMSNorm::RMSNorm(ggml_type type, u32 dimensions, f32 epsilon)
+    : epsilon_(epsilon)
+    ,weight_(type, {dimensions})
+{
+}
+
+RMSNorm::~RMSNorm()
+{
+}
+
+dnnl::memory RMSNorm::forward(dnnl::memory tensor)
+{
+}
+
+dnnl::memory RMSNorm::norm(dnnl::memory x)
+{
+}
+
 //--- Model
 //-----------------------------------------------------------
 Model::Model()
@@ -19,21 +548,27 @@ Llama2::Llama2()
 }
 
 Llama2::Llama2(const Config& config)
-    :config_(config)
+    : config_(config)
 {
 }
 
 Llama2::Llama2(Llama2&& other)
-    :network_(std::move(other.network_))
+    : network_(std::move(other.network_))
 {
 }
 
 Llama2::~Llama2()
 {
-	network_.clear();
+    network_.clear();
 }
 
 Llama2& Llama2::operator=(Llama2&& other)
 {
+    if(this == &other) {
+        return *this;
+    }
+    config_ = other.config_;
+    network_ = std::move(other.network_);
+    return *this;
 }
 } // namespace cppgpt
